4d18693793a82cf9eb62cc7f41aaa2b4c9daeb46
Rudra Chaudhari <rudravchaudhari167@gmail.com>
2026-09-15 22:16:31 +0530

    Time: 1 ms (99.94%), Space: 46.8 MB (64.91%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,9 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-
-        int result = 0;
-
-        for (int i = 0; i < nums.length; i++) {
+class Solution{
+    public int singleNumber(int [] nums){
+        int result = 0 ;
+        for(int i=0;i<nums.length;i++){
             result = result ^ nums[i];
         }
-
         return result;
     }
 }
